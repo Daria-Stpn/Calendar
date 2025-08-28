6bb9d54d0429c78ecec6ea01004749ead992c57b
Daria-Stpn <stepancukdar94@gmail.com>
2025-08-28 19:11:20 +0300

    Add creating events form

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import style from "./Main.module.scss";
-
+import AddForm from "../AddForm/AddForm";
+import { FaRegCalendarPlus } from "react-icons/fa";
 
 function Main(props) {
-    return <main className={style.wrapper}>Main</main>;
+    const [modalOpen, setModalOpen] = React.useState(false);
+    return (
+        <main className={style.wrapper}>
+            {modalOpen && <AddForm open={setModalOpen} />}
+            <button
+                className={style.addButton}
+                onClick={() => setModalOpen(true)}
+            >
+                <FaRegCalendarPlus />
+            </button>
+        </main>
+    );
 }
-
 
 export default Main;
